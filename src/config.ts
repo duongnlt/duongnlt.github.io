eecdeb5d706d603c2cec78b1b06911aa286b65f5
Duong Nguyen <duongnlt@Duongs-MacBook-Air.local>
2025-11-26 17:22:09 +0100

    Modify blog style

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 type Config = {
-    author: string
-    title: string
-    description: string
-    lang: string
-  }
-  
-  export default {
-    author: 'Samuel Breznjak',
-    title: 'Minimal - Astro blog theme',
-    description: 'Minimalistic blog theme for astro',
-    lang: 'en',
-  } satisfies Config
+  author: string
+  title: string
+  description: string
+  lang: string
+}
+
+export default {
+  author: 'Duong Nguyen',
+  title: 'Duong Nguyen',
+  description: 'Personal blog',
+  lang: 'en',
+} satisfies Config
